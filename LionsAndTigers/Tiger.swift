00da18a25e086315aa0138cc9558e6a54fb58b3f
ChangKH <web@kunhsiang.com>
2014-12-09 17:07:42 +0800

    First Add Project!

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by ChangKH on 2014/12/9.
+//  Copyright (c) 2014年 kunhsiang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
